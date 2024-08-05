e582c637e729edcc2fd72a5817b650a7b529c421
Dan Hearn <92724998+danhearn@users.noreply.github.com>
2024-08-05 10:53:39 +0100

    created_serial_handler

--- installation-application/main.py
+++ installation-application/main.py
@@ -3,19 +3,24 @@ from time import sleep
 import data_processor as dp
 import semantic_init
 import semantic_client
+import serial_com
 
 NUMBER_OF_VECTORS = 4
 CSV_PATH = '/Users/erika/Documents/GitHub/transforming-collections/installation-application/data/system-dataset-gif-test.csv'
 PURE_DATA_PATH = '/Users/erika/Documents/GitHub/data-sonification/semantic_synth(with-effects).pd'
 EMBEDDINGS_PATH = '/Users/erika/Documents/GitHub/transforming-collections/data/input/tate_wellcome_SEA_text_embeddings.npy'
+LED_MATRIX_PATH = '/dev/ttyACM0'
+ARUDUINO_PATH = ''
 DELAY = 5
 
 class MainProgram: 
-    def __init__(self, CSV_path, num_vectors, pure_data_path, embeddings_path, delay): 
+    def __init__(self, CSV_path, num_vectors, pure_data_path, embeddings_path, led_matrix_path, arduino_path, delay): 
         self.positive_client = semantic_client.SemanticClient("127.0.0.1", 9000)
         self.negative_client = semantic_client.SemanticClient("127.0.0.1", 10000)
         self.semantic_init = semantic_init.SemanticModel(pure_data_path, embeddings_path, num_vectors)
         self.data_processor = dp.DataProcessor(CSV_path, self.semantic_init)
+        self.LED_matrix = serial_com.SerialCommunication(led_matrix_path)
+        self.LED_matrix.connect_serial()
         self.delay = delay
         
     def run(self):
@@ -39,7 +44,7 @@ class MainProgram:
             
 if __name__ == "__main__":
     try: 
-        main_program = MainProgram(CSV_PATH, NUMBER_OF_VECTORS, PURE_DATA_PATH, EMBEDDINGS_PATH, DELAY)
+        main_program = MainProgram(CSV_PATH, NUMBER_OF_VECTORS, PURE_DATA_PATH, EMBEDDINGS_PATH, LED_MATRIX_PATH, ARUDUINO_PATH ,DELAY)
         main_program.run()
     except KeyboardInterrupt:
         main_program.semantic_init.pure_data.terminate()
