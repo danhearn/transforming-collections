53020f57aeafdeec996b15c82db4004379d05f94
lp407 <lukeplowden@gmail.com>
2024-08-18 17:43:54 +0100

    changed to multiprocessing

--- installation-application/main.py
+++ installation-application/main.py
@@ -3,6 +3,7 @@ from time import sleep
 import data_processor as dp
 import gif_player as gp
 
+from multiprocessing import Process, Queue
 import threading
 import queue
 
@@ -13,28 +14,27 @@ NUMBER_OF_VECTORS = 4
 class MainProgram: 
     def __init__(self, CSV_path, num_vectors): 
         self.data_processor = dp.DataProcessor(CSV_path, num_vectors)
-        self.queue = queue.Queue()
+        self.queue = Queue()
         self.gif_player = gp.GifPlayer("./data/gifs", self.queue)
-        self.gif_player_thread = threading.Thread(target=self.gif_player.run)
+        self.gif_player_thread = Process(target=self.gif_player.run)
         self.gif_player_thread.start()
     
     def run(self):
         while True:
-            if not self.queue.empty():
-                message = self.queue.get()
-                if message == "terminate":
-                    self.gif_player.terminate()
-            self.gif_player.impl_poll_events()
+            # if not self.queue.empty():
+            #     message = self.queue.get()
+            #     if message == "terminate":
+            #         self.gif_player.terminate()
             row = self.data_processor.get_random_row()
             print(f"Processing index: {row.name}, Countries: {row['Countries']}, Keywords: {row['Keywords']}") #, Label and vectors: {[row['Label']] +  list(row['Vectors'])}
             
             # this is just for testing, but I've added about 64 indexes with gifs
-            # for i in range(64):
-            #     if row['Gifs'] == f"gif-{i}":
-            #         print(f"Found gif-{i}")
-            #         self.queue.put(f"gif-{i}")
+            for i in range(64):
+                if row['Gifs'] == f"gif-{i}":
+                    print(f"Found gif-{i}")
+                    self.queue.put(f"gif-{i}")
 
-            # sleep(0.1) # you can change this, but in the actual running of the system there will be around 5 - 10 seconds of delay whilst the midi file is played
+            sleep(0.2) # you can change this, but in the actual running of the system there will be around 5 - 10 seconds of delay whilst the midi file is played
 
 if __name__ == "__main__":
     # Normal data set main_program = MainProgram('/Users/erika/Documents/GitHub/transforming-collections/prototype-scripts/app/data/tanc-etan_system-dataset.csv', NUMBER_OF_VECTORS)
