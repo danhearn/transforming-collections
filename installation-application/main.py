f46d7955122700ee0f7f82bcff56884754e4b56c
lp407 <lukeplowden@gmail.com>
2024-08-20 12:31:07 +0100

    works with new media_player class

--- installation-application/main.py
+++ installation-application/main.py
@@ -2,22 +2,25 @@ from time import sleep
 
 import data_processor as dp
 import gif_player as gp
+import media_player as mp
 
 from multiprocessing import Process, Queue
-import threading
-import queue
 
 NUMBER_OF_VECTORS = 4
+GIFS_PATH  = "./data/gifs/"
+VIDS_PATH = "./data/vids/"
 
 # please create a new module and class in the gif_player.py file and incoporate it into this program.
 
 class MainProgram: 
     def __init__(self, CSV_path, num_vectors): 
         self.data_processor = dp.DataProcessor(CSV_path, num_vectors)
-        self.queue = Queue()
-        self.gif_player = gp.GifPlayer("./data/gifs", self.queue)
-        self.gif_player_thread = Process(target=self.gif_player.run)
-        self.gif_player_thread.start()
+        # self.queue = Queue()
+        # self.gif_player = gp.GifPlayer(queue=self.queue)
+        # self.gif_player_thread = Process(target=self.gif_player.run)
+        # self.gif_player_thread.start()
+        self.media_player = mp.MediaPlayer(GIFS_PATH, VIDS_PATH)
+        self.media_player.start_on_new_process()
     
     def run(self):
         while True:
@@ -27,16 +30,12 @@ class MainProgram:
             #         self.gif_player.terminate()
             row = self.data_processor.get_random_row()
             print(f"Processing index: {row.name}, Countries: {row['Countries']}, Keywords: {row['Keywords']}") #, Label and vectors: {[row['Label']] +  list(row['Vectors'])}
-            
-            # this is just for testing, but I've added about 64 indexes with gifs
-            for i in range(64):
-                if row['Gifs'] == f"gif-{i}":
-                    print(f"Found gif-{i}")
-                    self.queue.put(f"gif-{i}")
-
-            sleep(0.2) # you can change this, but in the actual running of the system there will be around 5 - 10 seconds of delay whilst the midi file is played
+            if isinstance(row['Gifs'], str):
+                print(f"Found {row['Gifs']}")
+                self.media_player.queue_media(f"{row['Gifs']}")
+            sleep(1) # you can change this, but in the actual running of the system there will be around 5 - 10 seconds of delay whilst the midi file is played
 
 if __name__ == "__main__":
     # Normal data set main_program = MainProgram('/Users/erika/Documents/GitHub/transforming-collections/prototype-scripts/app/data/tanc-etan_system-dataset.csv', NUMBER_OF_VECTORS)
-    main_program = MainProgram('./data/system-dataset-gif-test.csv', NUMBER_OF_VECTORS)
+    main_program = MainProgram('./data/test.csv', NUMBER_OF_VECTORS)
     main_program.run()
