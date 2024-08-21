887aa56f5231588efd09515881563e8af0309bc6
Dan Hearn <dan@danhearndigital.com>
2024-08-21 11:06:03 +0100

    pathslib
    installed paths lib so this can work on other computers. Also added a mechanism that checks the os for puredata and uses the path to run puredata. This means it will work with different versions.

--- installation-application/semantic_init.py
+++ installation-application/semantic_init.py
@@ -1,3 +1,4 @@
+import glob
 import pandas as pd
 import numpy as np
 
@@ -9,10 +10,20 @@ import subprocess
 
 class SemanticModel: 
     def __init__(self, pure_data_path, embeddings_path, vector_length):
-        self.pd_executable = '/Applications/Pd-0.55-0.app/Contents/Resources/bin/pd'
+        self.pd_executable = self.find_pd_executable()
+        if not self.pd_executable:
+            raise Exception("Pure Data not found! Please install Pure Data!")
         self.pure_data = subprocess.Popen([self.pd_executable, "-nogui", pure_data_path])
         self.bert = np.load(embeddings_path)
         self.vector_length = vector_length
+    
+    def find_pd_executable(self):
+        pd_apps = glob.glob('/Applications/Pd-*.app/Contents/Resources/bin/pd')
+        
+        if pd_apps:
+            return pd_apps[0]
+        
+        return None
 
     def make_vectors(self):
         #normalise first
